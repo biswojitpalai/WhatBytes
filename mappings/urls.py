3ba9bca992fffbd002a46057df6c467347d428dc
Biswa-jeet <coderbiswa719@gmail.com>
2025-04-19 00:06:28 +0530

    url update

--- mappings/urls.py
+++ mappings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import MappingCreateListView, MappingByPatientView, MappingDeleteView
 
 urlpatterns = [
-    path('', MappingCreateListView.as_view()),                        # POST + GET /api/mappings/
-    path('<int:patient_id>/', MappingByPatientView.as_view()),        # GET /api/mappings/<patient_id>/
-    path('delete/<int:pk>/', MappingDeleteView.as_view()),            # DELETE /api/mappings/delete/<id>/
+    path('', MappingCreateListView.as_view()),                        
+    path('<int:patient_id>/', MappingByPatientView.as_view()),        
+    path('delete/<int:pk>/', MappingDeleteView.as_view()),           
 ]
